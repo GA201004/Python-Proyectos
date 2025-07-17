29a5c9045bd1a417f83b8e512096d09e7ff3b7bb
Gabriel <gabrieldavidochoa@gmail.com>
2025-07-16 22:08:23 -0400

    V3 Reloj Sin Interfaz

--- 11-reloj-con-sin-interfaz.py
+++ 11-reloj-con-sin-interfaz.py
@@ -3,17 +3,24 @@ import time
 def hora():
     Hora = time.strftime("%I:%M:%S:%p")
     print ("Hora actual=", Hora,end="\r",)
-    
 
 if __name__ == '__main__':
-    try:
-        while True:
-            hora()
-    
-    except KeyboardInterrupt:
-        Salir = input("¿Desea salir del programa? (s/n): ")
+    print("------Reloj sin interfaz gráfica------")
+    print("Presione Ctrl+C para detener el reloj.")
+    ejecutando = True
+    while True:
+        
+        try:
+            while ejecutando:
+                  hora()
+                  time.sleep(1)
+            
+        except KeyboardInterrupt:
+            Salir = input("¿Desea salir del programa? (s/n): ")
         if Salir.lower() == 's':
             print("Saliendo del programa.")
-        
+            ejecutando = False
+            break
+
         else:
-            print("Opción no válida. Continuando con el programa.")
+            print("------Continuando con el reloj------")
